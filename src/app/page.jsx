cb2f89f07ad38e745422ac8a0bbcfbb662de77c2
Prashant Mane <prashantmn47@gmail.com>
2024-09-28 15:13:13 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <>
+      <Link rel="stylesheet" href="/shiv">Click me</Link>
+    </>
+  );
+}
